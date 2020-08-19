bcdaf736ca29323e542047dcb80d3379864e6e59
letiiciia <leticialimaferreira2@gmail.com>
2020-08-19 19:22:03 -0300

    acrescentando conteúdos

---
+++ ex7_if_else.js
@@ -0,0 +1,11 @@
+const idade = 12
+
+// a palavra chave If(Se) - dentro dos parenteses vai a nossa condição
+if(idade >= 18){
+    //se a condição for verdadeira executa isso
+   console.log("Pode dirigir!")
+}else{
+    //palavra chave - se não
+    //se a condição for falsa executa isso
+   console.log("Pode jogar bola")
+}
---
+++ ex8_if_else.js
@@ -0,0 +1,7 @@
+const vacina = false
+if(vacina){
+    console.log("Liberada a aglomeração")
+}else{
+    console.log("Só no isolamento amada")
+}
+
